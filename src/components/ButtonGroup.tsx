5510ccfe45608ac69e27861d252944aebcd5d292
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 10:41:56 -0600

    fixed button text for sidebar button group

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -4,9 +4,9 @@ function ButtonGroup() {
   return (
     <section className="button-group">
       <Button variant="secondary">Mark all as complete</Button>
-      <Button variant="secondary">Mark all as complete</Button>
-      <Button variant="secondary">Mark all as complete</Button>
-      <Button variant="secondary">Mark all as complete</Button>
+      <Button variant="secondary">Mark all as incomplete</Button>
+      <Button variant="secondary">Reset to initial</Button>
+      <Button variant="secondary">Remove all items</Button>
     </section>
   );
 }
