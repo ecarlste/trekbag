d8ddcee3ccc6bd9612daf2bec8aa1aa92c9c5824
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 10:17:28 -0600

    added subcomponents for the main element

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,19 @@
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
+import Header from "./Header";
+import ItemList from "./ItemList";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <BackgroundHeading />
 
-      <main>Main Content</main>
+      <main>
+        <Header />
+        <ItemList />
+        <Sidebar />
+      </main>
 
       <Footer />
     </>
