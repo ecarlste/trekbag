f1caaa1cf8793b77cb883ddde1fae4ae9053716b
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 13:03:39 -0600

    set up AddItemForm input value to be controlled

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function AddItemForm() {
+  const [text, setText] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    console.log("Adding item:", text);
+    setText("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input />
+      <input value={text} onChange={handleChange} />
       <Button>Add to list</Button>
     </form>
   );
