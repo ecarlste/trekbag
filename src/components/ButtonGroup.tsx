6e64406165e55d8ce2dbe7b8342b0b2aa5bd8b10
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 10:54:09 -0600

    use array map to generate secondary buttons

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,12 +1,20 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
 function ButtonGroup() {
   return (
     <section className="button-group">
-      <Button variant="secondary">Mark all as complete</Button>
-      <Button variant="secondary">Mark all as incomplete</Button>
-      <Button variant="secondary">Reset to initial</Button>
-      <Button variant="secondary">Remove all items</Button>
+      {secondaryButtons.map((buttonText) => (
+        <Button key={buttonText} variant="secondary">
+          {buttonText}
+        </Button>
+      ))}
     </section>
   );
 }
