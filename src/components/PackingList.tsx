2cf83cd448d2ed1fe0d121ff912f55b2cc64296b
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 17:41:16 -0600

    implement sorting on the packing list by completed status

--- src/components/PackingList.tsx
+++ src/components/PackingList.tsx
@@ -1,6 +1,8 @@
+import Select from "react-select";
 import { PackingListItemType } from "../lib/types";
 import PackingListEmpty from "./PackingListEmpty";
 import PackingListItem from "./PackingListItem";
+import { useState } from "react";
 
 type PackingListProps = {
   items: PackingListItemType[];
@@ -8,16 +10,46 @@ type PackingListProps = {
   handleToggleItemPacked: (id: string) => void;
 };
 
+const sortingOptions = [
+  { value: "default", label: "Sort by Default" },
+  { value: "packed", label: "Sort by Packed" },
+  { value: "unpacked", label: "Sort by Unpacked" },
+];
+
 function PackingList({
   items,
   handleDeleteItem,
   handleToggleItemPacked,
 }: PackingListProps) {
+  const [sortBy, setSortBy] = useState(sortingOptions[0].value);
+
+  const sortedItems = [...items].sort((a) => {
+    if (sortBy === "packed") {
+      return a.isPacked ? -1 : 1;
+    }
+
+    if (sortBy === "unpacked") {
+      return a.isPacked ? 1 : -1;
+    }
+
+    return 0;
+  });
+
   return (
     <ul className="packing-list">
       {items.length === 0 && <PackingListEmpty />}
 
-      {items.map((item) => (
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+            onChange={(option) => setSortBy(option?.value || "default")}
+          />
+        </section>
+      )}
+
+      {sortedItems.map((item) => (
         <PackingListItem
           key={item.id}
           item={item}
