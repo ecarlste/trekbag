fb1d140faf8ad6ccc21bfde99ad55f385b275a23
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-05 10:14:31 -0600

    wired up stylesheet with existing components

--- src/components/BackgroundHeading.tsx
+++ src/components/BackgroundHeading.tsx
@@ -1,5 +1,5 @@
 function BackgroundHeading() {
-  return <h1>BackgroundHeading</h1>;
+  return <h1>trekbag</h1>;
 }
 
 export default BackgroundHeading;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <small>&copy; 2025. Copyright by TheCodeCartel. </small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
